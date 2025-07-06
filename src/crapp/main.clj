500949878c1eb6c7e2ba7d9174e537f5b6c5bda3
Keagan <keaganncollins@gmail.com>
2025-07-06 15:55:25 +0200

    implement system to build project with components

--- src/crapp/main.clj
+++ src/crapp/main.clj
@@ -3,30 +3,10 @@
             [crapp.utils :as utils]
             [crapp.config :as conf]))
 
-(defn create-project! [project-name template]
-  (println (str "Creating " template " project '" project-name "'..."))
-  (try
-    (let [project-path (str "./" project-name)
-          template-path (str (conf/read-value :templates :dir) "/" template)]
-      (utils/make-directory! project-path)
-      (template/deploy-template-files! template-path 
-                                       project-path 
-                                       {:project project-name}))
-    (catch Exception e
-      (println (.getMessage e)))
-    (finally 
-      (println "Done."))))
-
 (defn -main [& args]
-  (cond
-    (empty? args)
-    (println "Please provide a project name.")
+  ;;TODO: make a CLI that allows the user to choose:
+  ; - project name
+  ; - template
+  ; - one component from each component group for the chosen template
+  )
 
-    (= (count args) 1)
-    (create-project! (first args) "default")
-
-    (some #(= (last args) %) (conf/read-value :templates :list))
-    (create-project! (first args) (last args))
-
-    (> (count args) 2)
-    (println "Too many arguments.")))
